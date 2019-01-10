5097438edd38f9849352adde1326d17e9d5191dc
Luka Lavrnja <lavrnja.luka@gmail.com>
2019-01-11 00:43:36 +0100

    Vjerojatno krivo, ali pokusaj za login. Ne mogu testirat...

--- start.py
+++ start.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+from app import app, lm, baza
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from app.oauth import OAuthSignIn
+import requests
+
 import movie_statistic
 SECRET_KEY = '123'
 DEBUG = True
@@ -20,6 +26,50 @@ def profile():
 def trending():
     trending_movies = movie_statistic.get_trending()
     return render_template('trending.html', trending_movies = trending_movies)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@lm.user_loader   #User Loader Function - Flask needs the application's help in loading a user
+def load_user(id):
+    user = baza.db.Users.find_one({'user_id': int(id)})
+    return User(user['user_id'], user['email'], user['social_id'], user['first_name'], user['last_name'])
+
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+
+    oauth = OAuthSignIn.get_provider(provider)
+    user_id, social_id, first_name, last_name, email = oauth.callback()
+
+    if social_id is None:
+        flash('Authentication failed.')
+        return redirect(url_for('index'))
+
+    user = baza.db.Users.find_one({"social_id": social_id})
+
+    if not user:
+        baza.db.Users.insert_one({"user_id": int(user_id), "email": email, "social_id": social_id, "first_name": first_name, "last_name": last_name, "gradovi": ['Zagreb'], "filmovi" : [], 'favoriti': []})
+        user = baza.db.Users.find_one({"social_id": social_id})
+
+    login_user(User(user['user_id'], user['email'], user['social_id'], user['first_name'], user['last_name']), remember= True, force= True)
+    return redirect(url_for('index'))
+
+
+
+
 
 def main():
     app.run(host='0.0.0.0')
