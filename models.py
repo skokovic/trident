576ff598c8d56740d3c8f4613597d042eac1e9fa
Tomislav Skokovic <tomislav.skokovic@comsysto.com>
2019-01-12 18:23:25 +0100

    Add picture to user

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from flask_login import UserMixin
 
 class User(UserMixin):
-    def __init__(self, user_id, email, social_id, first_name, last_name, gender, location, age_range, likes):
+    def __init__(self, user_id, email, social_id, first_name, last_name, gender, location, age_range, likes, picture):
         self.id         = user_id
         self.email      = email
         self.social_id  = social_id
@@ -11,6 +11,7 @@ class User(UserMixin):
         self.location   = location
         self.age_range  = age_range
         self.likes      = likes 
+        self.picture    = picture
 
 #    social_id: a string that defines a unique identifier from the third party authentication service used to login.
 #    nickname: a nickname for the user. Must be defined for all users, and does not need to be unique.
